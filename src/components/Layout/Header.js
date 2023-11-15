f19253ad09aeabb40121818e8ef6f6094f11c256
Christian Barroso <barrosochristian3@gmail.com>
2023-11-15 15:47:00 -0800

    food order shit left for mobile

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
     return (
         <Fragment>
             <header className={classes.header}>
-                <h1>FoodOrder</h1>
+                <h1 className={classes.foodOrderH1}>FoodOrder</h1>
                 <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
